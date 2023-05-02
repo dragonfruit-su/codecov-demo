057694dec92462254aa89f72e04549e230e2a4eb
Azora Wolf <azorathewolf@gmail.com>
2023-05-02 10:59:22 +0200

    Add Sentry to the app

--- api/app.py
+++ api/app.py
@@ -2,8 +2,23 @@ from flask import (
     Flask,
     request,
 )
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 from calculator.calculator import Calculator
+
+app = Flask(__name__)
+
+sentry_sdk.init(
+    dsn="https://cfebadc1abce42949ffea57f12343aa5@o1420511.ingest.sentry.io/4505113561399296",
+    integrations=[
+        FlaskIntegration(),
+    ],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+)
 
 app = Flask(__name__)
 
@@ -26,6 +41,11 @@ def multiply():
 @app.route("/api/divide", methods=["POST"])
 def divide():
     return operation("divide", 2)
+
+
+@app.route("/debug-sentry")
+def trigger_error():
+    division_by_zero = 1 / 0
 
 
 def operation(method, num_factors):
